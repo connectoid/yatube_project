5b1b34f25932cf9d4ecbdcddac872ae434db703c
Александр Белей <beleysasha@gmail.com>
2021-12-01 19:24:46 +1200

    add render

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,10 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request, slug):
     return HttpResponse('Посты, отфильтрованные по группам')
